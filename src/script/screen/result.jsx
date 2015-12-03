6fdc63ec398aa9bb7da985b562339e455a6093e4
suitougreentea <suitougreentea@users.noreply.github.com>
2015-12-04 00:26:08 +0900

    Fix tweet button

--- src/script/screen/result.jsx
+++ src/script/screen/result.jsx
@@ -57,8 +57,11 @@ export default React.createClass({
     style.use()
   },
   componentDidMount() {
-    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs)}}(document, 'script', 'twitter-wjs');
-    //if(typeof twttr !== "undefined") twttr.widgets.load()
+    if(typeof twttr === "undefined") {
+      !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs)}}(document, 'script', 'twitter-wjs');
+    } else {
+      twttr.widgets.load()
+    }
   },
   componentWillUnmount() {
     style.unuse()
