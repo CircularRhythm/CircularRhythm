61561219bf6214f0bc6655f9f0955f59cd9ff106
suitougreentea <suitougreentea@users.noreply.github.com>
2015-12-11 20:46:11 +0900

    Use webpack-dev-server

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,30 +1,52 @@
 import webpack from "webpack";
 
-export default {
-  output: {
-    filename: 'main.js',
-    publicPath: 'asset/'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel'
-      },
-      {
-        test: /\.(json|bmson)$/,
-        loader: 'json-loader'
-      },
-      {
-        test: /\.sass$/,
-        loaders: ['style-loader/useable', 'css-loader', 'sass-loader?indentedSyntax']
-      }
-    ]
-  },
-  resolve: {
-    extensions: ["", ".js", ".jsx"]
-  },
-  plugins: [
-  ]
+// Type: "development", "production"
+export default function(type) {
+  const development = type == "development"
+  const production = type == "production"
+  const productionMin = type == "production-min"
+
+  const config = {
+    entry: ["./src/script/main.js"],
+    output: {
+      path: __dirname + "/build/asset/",
+      publicPath: "/asset/",
+      filename: "main.js",
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          loaders: ["babel"]
+        },
+        {
+          test: /\.json$/,
+          loader: "json-loader"
+        },
+        {
+          test: /\.sass$/,
+          loaders: ["style-loader/useable", "css-loader", "sass-loader?indentedSyntax"]
+        }
+      ]
+    },
+    resolve: {
+      extensions: ["", ".js", ".jsx"]
+    },
+    plugins: []
+  }
+
+  if(development) {
+    config.devtool = "cheap-module-eval-source-map"
+    config.debug = true
+    config.entry.push("webpack/hot/dev-server")
+    config.module.loaders[0].loaders.push("webpack-module-hot-accept")
+    config.plugins.push(new webpack.HotModuleReplacementPlugin())
+  }
+
+  if(productionMin) {
+    config.plugins.push(new webpack.optimize.UglifyJsPlugin())
+  }
+
+  return config
 }
