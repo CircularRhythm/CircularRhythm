37c5d2d2e7308f12fec94b5701e832271e6fc63e
suitougreentea <suitougreentea@users.noreply.github.com>
2016-02-04 18:09:21 +0900

    Add Util test

--- test/util_test.js
+++ test/util_test.js
@@ -2,7 +2,9 @@ import assert from "power-assert"
 import Util from "circular-rhythm/util"
 
 describe("Util", () => {
-  it("formatTime", () => {
+  it("can format time", () => {
     assert(Util.formatTime(10000) === "0:10")
+    assert(Util.formatTime(60000) === "1:00")
+    assert(Util.formatTime(999) === "0:00")
   })
 })
