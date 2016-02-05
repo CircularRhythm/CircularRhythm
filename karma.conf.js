383b4889b13267906304635c3d23478b89e2356a
suitougreentea <suitougreentea@users.noreply.github.com>
2016-02-05 17:50:40 +0900

    Coveralls

--- karma.conf.js
+++ karma.conf.js
@@ -44,11 +44,11 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
+    reporters: ['progress', 'coverage', 'coveralls'],
 
     coverageReporter: {
-      type: 'html',
-      dir: 'coverage',
+      type: 'lcov',
+      dir: 'coverage/',
       subdir: '.',
     },
 
